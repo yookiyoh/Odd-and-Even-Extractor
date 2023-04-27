c388052594939e4ab6d22bf43ae9f01779197a67
yookiyoh <ralphlorenzicodilan@iskolarngbayan.pup.edu.ph>
2023-04-27 12:36:27 +0800

    introductory headline comments

--- odd and even extractor.py
+++ odd and even extractor.py
@@ -1,1 +1,5 @@
-#
+# Codilan, Ralph Lorenz I.
+# BSCpE 1-5
+# Object-Oriented Programming | Assignment 4 - Problem 1
+# This program reads a text file that contains a set of integers
+# The odd and even integers would be extracted from the main text file inputs and transfer them into separate text files in categorization
