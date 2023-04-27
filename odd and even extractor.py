025f01866842a63cc1df78aedfec42757be76675
yookiyoh <ralphlorenzicodilan@iskolarngbayan.pup.edu.ph>
2023-04-27 12:40:42 +0800

    pseudocode

--- odd and even extractor.py
+++ odd and even extractor.py
@@ -3,3 +3,13 @@
 # Object-Oriented Programming | Assignment 4 - Problem 1
 # This program reads a text file that contains a set of integers
 # The odd and even integers would be extracted from the main text file inputs and transfer them into separate text files in categorization
+
+# open the numbers.txt file in write mode
+
+# open the numbers.txt file in read mode
+
+# open the even.txt file in write mode and write even numbers to it
+
+# open the odd.txt file in write mode and write odd numbers to it
+
+# print a goodbye message and terminate the program
